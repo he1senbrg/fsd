bcf20a134a64f13776fdad8febe2a14276b884f0
Iruos8805 <souris8805@gmail.com>
2026-04-25 18:29:27 +0530

    Integrate PWA support

---
+++ frontend/src/app/manifest.js
@@ -0,0 +1,25 @@
+export const dynamic = 'force-static'
+
+export default function manifest() {
+  return {
+    name: 'KalaSetu',
+    short_name: 'KalaSetu',
+    description: 'Bridging Tradition and Opportunity',
+    start_url: '/feed',
+    display: 'standalone',
+    background_color: '#fdfbf7',
+    theme_color: '#ffffff',
+    icons: [
+      {
+        src: '/icon-192x192.png',
+        sizes: '192x192',
+        type: 'image/png',
+      },
+      {
+        src: '/icon-512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+      },
+    ],
+  }
+}
